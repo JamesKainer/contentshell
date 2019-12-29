a9acef8529dfd754f25a3c33a8589ed1629f482e
James Kainer <47008209+JamesKainer@users.noreply.github.com>
2019-12-29 14:55:24 -0800

    Changed Reboot function for RACHEL-Pi
    The reboot function was not executing on the RACHEL-Pi. Added a new reboot function for it.

--- admin/hardware.php
+++ admin/hardware.php
@@ -24,7 +24,11 @@ if (isset($_POST['shutdown'])) {
 } else if (isset($_POST['reboot'])) {
     # pause so the webserver can respond, the complexity of the the
     # command is so that the exec() returns instantly
-    exec("sudo sh -c 'sleep 3; /sbin/reboot;' > /dev/null 2>&1 &", $exec_out, $exec_err);
+    if (is_rachelplus()) {
+        exec("sudo sh -c 'sleep 3; /sbin/reboot;' > /dev/null 2>&1 &", $exec_out, $exec_err);
+    } else if (is_rachelpi()) {
+        exec("sudo reboot now", $exec_out, $exec_err);
+    }  
     if ($exec_err) {
         echo $lang['restart_failed'];
     } else {
